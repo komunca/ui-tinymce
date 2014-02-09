f274851d5971454d0b41e17d46e9d37eb234e279
komunca <komunca@gmail.com>
2014-02-09 20:28:39 +0100

    Update tinymce.js
    Version for tiny mc 3.5.8

--- src/tinymce.js
+++ src/tinymce.js
@@ -1,78 +1,81 @@
 /**
  * Binds a TinyMCE widget to <textarea> elements.
  */
-angular.module('ui.tinymce', [])
-  .value('uiTinymceConfig', {})
-  .directive('uiTinymce', ['uiTinymceConfig', function (uiTinymceConfig) {
+angular.module('ui.tinymce', []).value('uiTinymceConfig', {}).directive('uiTinymce', [ 'uiTinymceConfig', function(uiTinymceConfig){
     uiTinymceConfig = uiTinymceConfig || {};
     var generatedIds = 0;
     return {
-      require: 'ngModel',
-      link: function (scope, elm, attrs, ngModel) {
-        var expression, options, tinyInstance,
-          updateView = function () {
-            ngModel.$setViewValue(elm.val());
-            if (!scope.$root.$$phase) {
-              scope.$apply();
+        require : 'ngModel',
+        link : function(scope, elm, attrs, ngModel){
+            var expression, options, tinyInstance, updateView = function(){
+                ngModel.$setViewValue(elm.val());
+                if (!scope.$root.$$phase) {
+                    scope.$apply();
+                }
+            };
+            // generate an ID if not present
+            if (!attrs.id) {
+                attrs.$set('id', 'uiTinymce' + generatedIds++);
             }
-          };
-        // generate an ID if not present
-        if (!attrs.id) {
-          attrs.$set('id', 'uiTinymce' + generatedIds++);
-        }
 
-        if (attrs.uiTinymce) {
-          expression = scope.$eval(attrs.uiTinymce);
-        } else {
-          expression = {};
-        }
-        options = {
-          // Update model when calling setContent (such as from the source editor popup)
-          setup: function (ed) {
-            var args;
-            ed.on('init', function(args) {
-              ngModel.$render();
-            });
-            // Update model on button click
-            ed.on('ExecCommand', function (e) {
-              ed.save();
-              updateView();
-            });
-            // Update model on keypress
-            ed.on('KeyUp', function (e) {
-              ed.save();
-              updateView();
-            });
-            // Update model on change, i.e. copy/pasted text, plugins altering content
-            ed.on('SetContent', function (e) {
-              if(!e.initial){
-                ed.save();
-                updateView();
-              }
-            });
-            if (expression.setup) {
-              scope.$eval(expression.setup);
-              delete expression.setup;
+            if (attrs.uiTinymce) {
+                expression = scope.$eval(attrs.uiTinymce);
             }
-          },
-          mode: 'exact',
-          elements: attrs.id
-        };
-        // extend options with initial uiTinymceConfig and options from directive attribute value
-        angular.extend(options, uiTinymceConfig, expression);
-        setTimeout(function () {
-          tinymce.init(options);
-        });
+            else {
+                expression = {};
+            }
+            options = {
+                // Update model when calling setContent (such as from the source
+                // editor popup)
+                mode : 'exact',
+                elements : attrs.id
+            };
+            // extend options with initial uiTinymceConfig and options from
+            // directive attribute value
+            angular.extend(options, uiTinymceConfig, expression);
+            // This should be called after doing angular.extend otherwise it
+            // gets replaced by the one that's inside express
+            options.setup = function(ed){
+                var args;
 
+                ed.onInit.add(function(ed, cmd, ui, val){
+                    ngModel.$render();
+                });
 
-        ngModel.$render = function() {
-          if (!tinyInstance) {
-            tinyInstance = tinymce.get(attrs.id);
-          }
-          if (tinyInstance) {
-            tinyInstance.setContent(ngModel.$viewValue || '');
-          }
-        };
-      }
+                ed.onExecCommand.add(function(ed, cmd, ui, val){
+                    ed.save();
+                    updateView();
+                });
+
+                ed.onKeyUp.add(function(ed, cmd, ui, val){
+                    ed.save();
+                    updateView();
+                });
+
+                ed.onSetContent.add(function(ed, cmd, ui, val){
+                    if (!cmd.initial) {
+                        ed.save();
+                        updateView();
+                    }
+                });
+
+                if (expression != undefined && expression.setup) {
+                    expression.setup(ed);
+                    delete expression.setup;
+                }
+            };
+            setTimeout(function(){
+                tinymce.init(options);
+            }, 100);
+
+            ngModel.$render = function(){
+                if (!tinyInstance) {
+                    tinyInstance = tinymce.get(attrs.id);
+                }
+                if (tinyInstance) {
+                    tinyInstance.setContent(ngModel.$viewValue || '');
+                }
+            };
+        }
     };
-  }]);
+} ]);
